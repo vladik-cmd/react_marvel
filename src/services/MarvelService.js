8bbd96cc728510355df2068612f1479b3bb67cf4
Vladik <trushovvladislav@gmail.com>
2021-10-22 13:34:44 +0300

    add hooks, charList

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -23,6 +23,7 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
       description: char.description ? `${char.description.slice(0, 210)}...` : 'No description',
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
